389b520e5820b3410fdaa8e02953e6de27ec88bb
Unknown <baconomist@gmail.com>
2020-01-20 18:25:31 -0500

    Fix main.h for okapi

--- main.h
+++ main.h
@@ -39,7 +39,7 @@
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
@@ -50,9 +50,10 @@
  * concurrently! The okapi namespace will export all symbols inside the pros
  * namespace.
  */
-// using namespace pros;
-// using namespace pros::literals;
-// using namespace okapi;
+//using namespace pros;
+//using namespace pros::literals;
+using namespace okapi;
+using namespace okapi::literals;
 
 /**
  * Prototypes for the competition control tasks are redefined here to ensure
